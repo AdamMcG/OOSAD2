d42bfafecfed0d38a18fe5f97c1e3f4fd6142b99
Adam McGivern <adammcgivern@outlook.com>
2015-01-29 12:36:45 +0000

    Week 1 VPP work
    the demo of VPP we did in week 1

---
+++ Demo1/Example.java
@@ -0,0 +1,52 @@
+package Demo1;
+
+public class Example {
+
+	private string name = no name;
+	private string type = no type;
+	private double duration = 1.5;
+
+	public Example() {
+		// TODO - implement Example.Example
+		throw new UnsupportedOperationException();
+	}
+
+	public void getName() {
+		// TODO - implement Example.getName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(int name) {
+		// TODO - implement Example.setName
+		throw new UnsupportedOperationException();
+	}
+
+	public string getType() {
+		return this.type;
+	}
+
+	/**
+	 * 
+	 * @param type
+	 */
+	public void setType(string type) {
+		this.type = type;
+	}
+
+	public double getDuration() {
+		return this.duration;
+	}
+
+	/**
+	 * 
+	 * @param duration
+	 */
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+
+}
